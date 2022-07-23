f11eaf1311d92db71df05e0d5735cc0399a40345
jam <jamis404@163.com>
2022-07-24 00:04:31 +0800

    使用 const {size, ...rest} = context.attrs 将属性分开

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,13 +1,20 @@
 <template>
-<button>
-  <slot></slot>
-</button>
+  <div :size="size">
+    <button v-bind="rest">
+      <slot></slot>
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
-name: "Button"
-}
+  inheritAttrs: false,
+  setup(props, context) {
+    // ES6 剩余操作符
+    const {size, ...rest} = context.attrs;
+    return {size, rest};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
