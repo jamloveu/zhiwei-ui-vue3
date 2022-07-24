18821b532619af8cab20222e1c61f29805e828f1
jam <jamis404@163.com>
2022-07-24 23:20:00 +0800

    完善Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,12 +1,48 @@
 <template>
-  <h1>Dialog 示例</h1>
-  <div>
-    <Dialog />
+  <div>Dialog 示例</div>
+  <h1>示例1</h1>
+  <div style="position: relative; z-index: 1;">
+    <Button @click="toggle">toggle</Button>
+
+
+    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+      <template v-slot:content>
+        <strong>hi</strong>
+        <div>hi2</div>
+      </template>
+      <template v-slot:title>
+        <strong>加粗的标题</strong>
+      </template>
+    </Dialog>
   </div>
 </template>
+
 <script lang="ts">
-import Dialog from "../lib/Dialog.vue"
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import {
+  ref
+} from 'vue'
 export default {
-  components:{Dialog}
+  components: {
+    Dialog,
+    Button
+  },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    const f1 = () => {
+      return false
+    }
+    const f2 = () => {}
+    return {
+      x,
+      toggle,
+      f1,
+      f2
+    }
+  }
 }
 </script>
