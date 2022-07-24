fdeb55ab631539fb9ac143d09a37ae9e8398a4d3
jam <jamis404@163.com>
2022-07-24 14:32:35 +0800

    button组件完善，加level、disabled、loading

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,43 +1,55 @@
 <template>
-    <button class="zhiwei-button" :class="classes">
-      <slot></slot>
-    </button>
+  <button class="zhiwei-button" :class="classes">
+    <span v-if="loading" class="zhiwei-loadingIndicator"></span>
+    <slot />
+  </button>
 </template>
-
 <script lang="ts">
-import { computed } from 'vue'
+import { computed } from "vue";
 export default {
-  props:{
-    theme:{
-      type:String,
-      default: 'button'
-    },
-    size:{
+  props: {
+    theme: {
       type: String,
-      default: 'normal'
+      default: "button",
+    },
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
   },
-  setup(props){
-    const { theme,size } = props
-    const classes = computed(()=>{
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
       return {
-        [`zhiwei-theme-${theme}`]:theme,
-        [`zhiwei-size-${size}`]:size
-      }
-    })
-    return {
-      classes
-    }
-  }
+        [`zhiwei-theme-${theme}`]: theme,
+        [`zhiwei-size-${size}`]: size,
+        [`zhiwei-level-${level}`]: level
+      };
+    });
+    return { classes };
+  },
 };
 </script>
-
 <style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .zhiwei-button {
   box-sizing: border-box;
   height: $h;
@@ -52,6 +64,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -79,6 +92,7 @@ $radius: 4px;
     border-color: transparent;
     box-shadow: none;
     color: inherit;
+    background-color: transparent;
     &:hover,
     &:focus {
       background: darken(white, 5%);
@@ -94,10 +108,82 @@ $radius: 4px;
     height: 20px;
     padding: 0 4px;
   }
-  &.zhiwei-size-mini {
-    font-size: 10px;
-    height: 16px;
-    padding: 0 2px;
+  &.zhiwei-theme-button {
+    &.zhiwei-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.zhiwei-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
   }
+  &.zhiwei-theme-link {
+    &.zhiwei-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.zhiwei-theme-text {
+    &.zhiwei-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.zhiwei-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.zhiwei-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.zhiwei-theme-link, &.zhiwei-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+  > .zhiwei-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: zhiwei-spin 1s infinite linear;
+  }
+}
+@keyframes zhiwei-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
 }
 </style>
