f34945e88151c5bfcf5acb8bd10f6998dd45d684
jam <jamis404@163.com>
2022-08-26 15:27:02 +0800

    switch组件的基本样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,13 +1,49 @@
 <script setup lang="ts">
-
+import {ref} from "vue";
+let status = ref(false)
+const onClick = ()=>{
+  status.value = !status.value
+}
 </script>
 
 <template>
-<div>
-  switch
-</div>
+  <div class="zw-switch" :class="status?'zw-switch-on':''" @click="onClick">
+    <span class="node"></span>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+@import "style/var.scss";
 
+.zw-switch {
+  width: $switch-width;
+  height: $switch-height;
+  border: $switch-border-color;
+  border-radius: $switch-border-radius;
+  background-color: $switch-background-color;
+  position: relative;
+
+  .node {
+    width: 18px;
+    height: 18px;
+    border: $switch-border-color;
+    background-color: #fff;
+    display: inline-block;
+    border-radius: 50%;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    transition: transform $switch-transition-duration cubic-bezier(0.3, 1.05, 0.4, 1.05);
+  }
+
+  &-on{
+    background-color: #1989fa;
+    box-shadow: 0 0 0 2px rgba(45,140,240,.2);
+
+    .node{
+      transform: translateX($switch-width - 18px - 4px);
+    }
+  }
+
+}
 </style>
